11e3010ce03c9b2f4b89e9ca80ade156c6a7f64f
xuexiaoyu <xuexiaoyu@corp.netease.com>
2018-03-29 10:08:16 +0800

    Initial commit

---
+++ Example/EMNOSLib/EMViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMViewController.h
+//  EMNOSLib
+//
+//  Created by xuexiaoyu on 03/29/2018.
+//  Copyright (c) 2018 xuexiaoyu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface EMViewController : UIViewController
+
+@end
---
+++ Example/EMNOSLib/EMViewController.m
@@ -0,0 +1,29 @@
+//
+//  EMViewController.m
+//  EMNOSLib
+//
+//  Created by xuexiaoyu on 03/29/2018.
+//  Copyright (c) 2018 xuexiaoyu. All rights reserved.
+//
+
+#import "EMViewController.h"
+
+@interface EMViewController ()
+
+@end
+
+@implementation EMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
